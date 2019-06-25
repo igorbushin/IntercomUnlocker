7df3b705d384c08d852739d7f9644f62ef99d8c9
igorbushin <ingvar.gorbushin@gmail.com>
2019-06-25 23:54:41 +0400

    Update intercomUnlocker.ino

--- intercomUnlocker.ino
+++ intercomUnlocker.ino
@@ -4,7 +4,23 @@
 OneWire ibutton (pin); // I button connected on PIN 2.
 
 byte addr[8]; //array to store the Ibutton ID.
-
+byte newID0 [8] = {0x01, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x2F}; // - проверен (метаком, цифрал, визит).
+byte newID1 [8] = {0x01, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x9B}; // - проверен (кондор, цифрал, визит) - выходит в сервис!!!.
+byte newID2 [8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x14}; // - проверен (метаком, цифрал).
+byte newID3 [8] = {0x01, 0x53, 0xD4, 0xFE, 0x00, 0x00, 0x7E, 0x88}; // - проверен (метаком, цифрал).
+byte newID4 [8] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3D}; // - проверен (цифрал).
+byte newID5 [8] = {0x01, 0x53, 0xD4, 0xFE, 0x00, 0x00, 0x00, 0x6F}; // - проверен (визит).
+byte newID6 [8] = {0x01, 0xBE, 0x40, 0x11, 0x5A, 0x36, 0x00, 0xE1}; // - проверен (визит).
+byte newID7 [8] = {0x01, 0x00, 0x36, 0x5A, 0x11, 0x40, 0xBE, 0xB1}; // - проверен (визит).
+byte newID8 [8] = {0x01, 0x00, 0x00, 0x00, 0xFE, 0xD4, 0x53, 0x1E}; // - проверен (визит).
+byte newID9 [8] = {0x01, 0xBE, 0x40, 0x11, 0x0A, 0x00, 0x00, 0x1D}; // - проверен работает Визит иногда КЕЙМАНЫ
+//byte newID[8] = {0x01, 0x00, 0xBE, 0x11, 0xAA, 0x00, 0x00, 0xFB); //???-домофоны Кейман (KEYMAN)
+//byte newID[8] = {0x01, 0x76, 0xB8, 0x2E, 0x0F, 0x00, 0x00, 0x5C); //???-домофоны Форвард
+//byte newID[8] = {0x01, 0xBE, 0x40, 0x11, 0x5A, 0x56, 0x00, 0xBB}; //- проверен работает
+//byte newID[8] = {0x01, 0xBE, 0x40, 0x11, 0x00, 0x00, 0x00, 0x77}; //- проверен работает
+//byte newID[8] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x90, 0x19, 0xFF); //???-Отлично работает на старых домофонах
+//byte newID[8] = {0x01, 0x6F, 0x2E, 0x88, 0x8A, 0x00, 0x00, 0x4D); //???-Открывать что-то должен
+    
 void setup(){
  Serial.begin(115200); 
 }
@@ -28,8 +44,26 @@ void loop(){
   crc = ibutton.crc8(addr, 7);
   Serial.print("CRC: ");
   Serial.println(crc,HEX);
-  if ( Serial.read() == 'w' ){
-    ibutton.skip();ibutton.reset();ibutton.write(0x33);
+  char input = Serial.read();
+ 
+  switch(input) {
+    case '0': writeKey(newID0); break;
+    case '1': writeKey(newID1); break;
+    case '2': writeKey(newID2); break;
+    case '3': writeKey(newID3); break;
+    case '4': writeKey(newID4); break;
+    case '5': writeKey(newID5); break;
+    case '6': writeKey(newID6); break;
+    case '7': writeKey(newID7); break;
+    case '8': writeKey(newID8); break;
+    case '9': writeKey(newID9); break;
+  }
+}
+
+void writeKey(byte newID[8]) {
+    ibutton.skip();
+    ibutton.reset();
+    ibutton.write(0x33);
     Serial.print("  ID before write:");
     for (byte x=0; x<8; x++){
       Serial.print(' ');
@@ -41,37 +75,15 @@ void loop(){
     // send 0xD1
     ibutton.write(0xD1);
     // send logical 0
-    digitalWrite(10, LOW); pinMode(10, OUTPUT); delayMicroseconds(60);
-    pinMode(10, INPUT); digitalWrite(10, HIGH); delay(10);
+    digitalWrite(pin, LOW); pinMode(pin, OUTPUT); delayMicroseconds(60);
+    pinMode(pin, INPUT); digitalWrite(pin, HIGH); delay(10);
     
     Serial.print('\n');
     Serial.print("  Writing iButton ID:    ");
-    // Hardcode here your desired ID //
-    // FF:FF:FF:FF:FF:FF - metakom - 0x01, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x2F CRC:2F
-    // 00:36:5A:11:40:BE - visit 1 - 0x01, 0x00, 0x36, 0x5A, 0x11, 0x40, 0xBE, 0xB1 CRC:B1
-    // 00:00:00:FE:D4:53 - visit 2 - 0x01, 0x00, 0x00, 0x00, 0xFE, 0xD4, 0x53, 0x1E CRC:1E
-    // 00:00:00:00:00:00 - cyfral  - 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3D CRC:3D
-    //byte newID[8] = {0x01, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x2F}; //(ЩАС КОД НА VIZIT)
-    //byte newID[8] = {0x01, 0xBE, 0x40, 0x11, 0x5A, 0x36, 0x00, 0xE1}; //– код универсального ключа, для Vizit
-    //byte newID[8] = {0x01, 0xBE, 0x40, 0x11, 0x5A, 0x56, 0x00, 0xBB}; //- проверен работает
-    //byte newID[8] = {0x01, 0xBE, 0x40, 0x11, 0x00, 0x00, 0x00, 0x77}; //- проверен работает
-    //byte newID[8] = {0x01, 0xBE, 0x40, 0x11, 0x0A, 0x00, 0x00, 0x1D}; //- проверен работает Визит иногда КЕЙМАНЫ
-    byte newID[8] = {0x01, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x2F}; //- проверен(метаком, цифрал, ВИЗИТ).
-    //byte newID[8] = {0x01, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x9B}; //- проверен Визит, Метакомы, КОНДОР - выходит в сервис!!!.
-    //byte newID[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x14); //???-Открываает 98% Метаком и некоторые Цифрал
-    //byte newID[8] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x90, 0x19, 0xFF); //???-Отлично работает на старых домофонах
-    //byte newID[8] = {0x01, 0x6F, 0x2E, 0x88, 0x8A, 0x00, 0x00, 0x4D); //???-Открывать что-то должен
-    //byte newID[8] = {0x01, 0x53, 0xD4, 0xFE, 0x00, 0x00, 0x7E, 0x88); //???-Cyfral, Metakom
-    //byte newID[8] = {0x01, 0x53, 0xD4, 0xFE, 0x00, 0x00, 0x00, 0x6F); //???-домофоны Визит (Vizit) - до 99%
-    //byte newID[8] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3D); //???-домофоны Cyfral CCD-20 - до 70%
-    //byte newID[8] = {0x01, 0x00, 0xBE, 0x11, 0xAA, 0x00, 0x00, 0xFB); //???-домофоны Кейман (KEYMAN)
-    //byte newID[8] = {0x01, 0x76, 0xB8, 0x2E, 0x0F, 0x00, 0x00, 0x5C); //???-домофоны Форвард
-    writeV2(ibutton, newID);                  
-   
-  }
+    writeV1(newID);                  
 } 
 
-void writeV1(OneWire ibutton, byte newID[8]) {
+void writeV1(byte newID[8]) {
     ibutton.skip();
     ibutton.reset();
     ibutton.write(0xD5);
@@ -89,19 +101,19 @@ void writeV1(OneWire ibutton, byte newID[8]) {
     pinMode(10, INPUT); digitalWrite(10, HIGH); delay(10);
 }
 
-void writeV2(OneWire ds, byte key_to_write[8]) {
+void writeV2(byte key_to_write[8]) {
   for (uint8_t i = 0; i < 8; i++)
   {
     byte data[4];
-    ds.reset();
+    ibutton.reset();
     data[0] = 0x3C; // "WRITE" command
     data[1] = i; // programming i-th byte
     data[2] = 0;
     data[3] = key_to_write[i];
-    ds.write_bytes(data, 4);
+    ibutton.write_bytes(data, 4);
     Serial.print(".");
   
-    uint8_t crc = ds.read();
+    uint8_t crc = ibutton.read();
     
     if (OneWire::crc8(data, 4) != crc) {
         Serial.print("error!\r\n");
@@ -110,21 +122,17 @@ void writeV2(OneWire ds, byte key_to_write[8]) {
     else
       Serial.print(".");
     
-    send_programming_impulse();
+    //send_programming_impulse();
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH); 
+    delay(60);
+    digitalWrite(pin, LOW); 
+    delay(5);
+    digitalWrite(pin, HIGH); 
+    delay(50); 
   }
   
   Serial.println("done!");
-}
-
-void send_programming_impulse()
-{
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, HIGH); 
-  delay(60);
-  digitalWrite(pin, LOW); 
-  delay(5);
-  digitalWrite(pin, HIGH); 
-  delay(50); 
 }
 
 int writeByte(byte data){
